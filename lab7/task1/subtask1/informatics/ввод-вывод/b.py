4648c1e4772f030476928ac4bafc59997a4e6c2e
Arailym Yermekali <98966955+arraxeess@users.noreply.github.com>
2023-03-26 19:20:59 +0600

    Add files via upload

---
+++ lab7/task1/subtask1/informatics/ввод-вывод/b.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+next = 'The next number for the number {} is {}.'.format(a, a + 1)
+prev = 'The previous number for the number {} is {}.'.format(a, a - 1)
+print(next)
+print(prev)
+
+
---
+++ lab7/task1/subtask1/informatics/циклы/цикл while/a.py
@@ -0,0 +1,8 @@
+i = 1
+n = int(input()) 
+while i * i <= n:
+
+   print(i * i, ' ')
+
+   i = i + 1
+
